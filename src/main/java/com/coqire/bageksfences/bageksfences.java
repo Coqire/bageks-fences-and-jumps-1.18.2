0f5aaa3178203eb0305965697ebe331fc6ce0419
Coqire <megen.horne@hotmail.com>
2024-08-13 05:40:04 +1000

    - Added class

---
+++ src/main/java/com/coqire/bageksfences/bageksfences.java
@@ -0,0 +1,33 @@
+package com.coqire.bageksfences;
+
+import com.coqire.bageksfences.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(bageksfences.Mod_ID)
+public class bageksfences {
+    public static final String Mod_ID = "bageksfences";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public bageksfences() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
